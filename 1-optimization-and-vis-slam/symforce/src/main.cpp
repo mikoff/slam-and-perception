91494bc46429892918edb76789c8aea3e8454290
Aleksandr Mikov <sasha.mikoff@gmail.com>
2026-03-16 22:26:10 +0100

    adapt .gitignore

--- 1-optimization-and-vis-slam/symforce/src/main.cpp
+++ 1-optimization-and-vis-slam/symforce/src/main.cpp
@@ -1,6 +1,14 @@
+#include <symforce/opt/values.h>
+#include <symforce/opt/optimizer.h>
 #include <iostream>
 
 int main() {
-    std::cout << "C++ environment initialized." << std::endl;
+    sym::Values<double> values;
+    values.Set('x', 10.0);
+
+    std::cout << "SymForce C++ Optimizer is linked and ready!" << std::endl;
+    std::cout << "Stored initial guess for 'x': " << values.At<double>('x') << std::endl;
+
+
     return 0;
 }
